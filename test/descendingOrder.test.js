99680af6124b55679b25b16de3a367d60231fd3b
Tahir J <icodejs@gmail.com>
2016-04-13 16:20:05 +0100

    Update assertion

--- test/descendingOrder.test.js
+++ test/descendingOrder.test.js
@@ -4,7 +4,7 @@ import fn from '../src/descendingOrder.js';
 const task = 'Take any non-negative integer as a argument and return it with it\'s digits in descending order';
 
 describe(task, () => {
-  it('0 -> 0', () => assert.equal(fn(0), 0));
+  it('0 -> 0', () => assert.strictEqual(fn(0), 0));
   it('145263 -> 654321', () => assert.strictEqual(fn(145263), 654321));
   it('238947 -> 987432', () => assert.strictEqual(fn(238947), 987432));
   it('123456789 -> 987654321', () => assert.strictEqual(fn(123456789), 987654321));
