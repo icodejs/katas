a6309676fa001f48340a6de4b7943fa8a9677c0a
Tahir J <icodejs@gmail.com>
2016-04-18 10:57:02 +0100

    Add solution as comment

--- src/tortoiseRacing.js
+++ src/tortoiseRacing.js
@@ -14,6 +14,22 @@
 
  */
 
+/*
+  // Possible solution
+  function race(v1, v2, g){
+    let time=g/(v2-v1);
+    return v2>v1 ? [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)] : null;
+  }
+
+  function race(v1, v2, g) {
+      if (v1 >= v2){
+        return null;
+      }
+      var result = new Date(Math.floor(g / (v2 - v1) * 3600) * 1000);
+      return [result.getHours(), result.getMinutes(), result.getSeconds()];
+  }
+ */
+
 
 function race(v1, v2, g) {
   if (v1 > v2) return null;
