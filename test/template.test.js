087ce3b5b5ebd30ef8b3f7d60517aed4bb88e690
Tahir J <icodejs@gmail.com>
2016-05-05 17:12:26 +0100

    Added template test file

---
+++ test/template.test.js
@@ -0,0 +1,13 @@
+/*
+import assert from 'assert';
+import fn from '../src/fn.js';
+
+describe('function fn', () => {
+  it('should', () => {
+    assert.equal(fn(10), 19);
+    assert.deepEqual(fn([1, 2, 3, 4, 5]), [4, 9, 16, 25, 36]);
+    assert.strictEqual(fn(145263), 654321);
+    assert.deepStrictEqual(fn(3, 12), [12, 12, 12]);
+  });
+});
+*/
