f6b39de427b76c65ae2d799e6bf387a63611071e
Tahir J <icodejs@gmail.com>
2016-05-18 11:38:38 +0100

    More regex lessons

--- test/regex.test.js
+++ test/regex.test.js
@@ -3,6 +3,7 @@
 import assert from 'assert';
 
 describe('Regex functions', () => {
+  // http://regexone.com/lesson/introduction_abcs
   it('should match alphas', () => {
     assert.equal(/a/.test('abc'), true);
     assert.equal(/abc/.test('abc'), true);
@@ -10,6 +11,7 @@ describe('Regex functions', () => {
   });
 
   it('should match numbers', () => {
+    // http://regexone.com/lesson/letters_and_digits
     assert.equal(/1/.test(123), true);
     assert.equal(/123/.test(123), true);
     assert.equal(/1234/.test(123), false);
@@ -18,6 +20,7 @@ describe('Regex functions', () => {
   });
 
   it('should match using wildcard', () => {
+    // http://regexone.com/lesson/wildcards_dot
     assert.equal(/...\./.test('abc.'), true);
     assert.equal(/...\./.test('123.'), true);
     assert.equal(/...\./.test('ewfw'), false);
@@ -26,6 +29,7 @@ describe('Regex functions', () => {
   });
 
   it('should match specific characters', () => {
+    // http://regexone.com/lesson/matching_characters
     assert.equal(/[cfm]/.test('can'), true);
     assert.equal(/[cfm]/.test('man'), true);
     assert.equal(/[cfm]/.test('dan'), false);
@@ -45,6 +49,7 @@ describe('Regex functions', () => {
   });
 
   it('should exclude specific characters', () => {
+    // http://regexone.com/lesson/excluding_characters
     assert.equal(/[^b]og/.test('hog'), true);
     assert.equal(/[^b]og/.test('dog'), true);
     assert.equal(/[^b]og/.test('bog'), false);
@@ -55,6 +60,7 @@ describe('Regex functions', () => {
   });
 
   it('should match character ranges', () => {
+    // http://regexone.com/lesson/character_ranges
     assert.equal(/[A-Zd-w]/.test('Ana'), true);
     assert.equal(/[A-Zd-w]/.test('Bob'), true);
     assert.equal(/[A-Zd-w]/.test('aax'), false);
@@ -64,6 +70,33 @@ describe('Regex functions', () => {
     assert.equal(/[A-C][n-p][a-c]/.test('Bob'), true);
     assert.equal(/[A-C][n-p][a-c]/.test('aax'), false);
     assert.equal(/[A-C][n-p][a-c]/.test('bby'), false);
+  });
+
+  it('should match the a range of characters', () => {
+    // http://regexone.com/lesson/repeating_characters
+    assert.equal(/zzz{1,3}/.test('wazzzzup'), true);
+    assert.equal(/zzz{1,3}/.test('wazzzup'), true);
+    assert.equal(/zzz{1,3}/.test('wazup'), false);
+
+    assert.equal(/waz{2,4}up/.test('wazzzzup'), true);
+    assert.equal(/waz{2,4}up/.test('wazzzup'), true);
+    assert.equal(/waz{2,4}up/.test('wazup'), false);
+  });
+
+  it('should match an arbitrary number of characters', () => {
+    // http://regexone.com/lesson/kleene_operators?
+    assert.equal(/a*[bc]+/.test('aaaabcc'), true);
+    assert.equal(/a*[bc]+/.test('aabbbbc'), true);
+    assert.equal(/a*[bc]+/.test('aacc'), true);
+    assert.equal(/a*[bc]+/.test('a'), false);
+  });
+
+  it('should match either zero or one of the preceding character or group', () => {
+    // http://regexone.com/lesson/optional_characters
+    assert.equal(/[\d]+ files? found\?/.test('1 file found?'), true);
+    assert.equal(/[\d]+ files? found\?/.test('2 files found?'), true);
+    assert.equal(/[\d]+ files? found\?/.test('24 files found?'), true);
+    assert.equal(/[\d]+ files? found\?/.test('No files found.'), false);
   });
 });
 
